b39d55b30d75ca402f4dc538367c59a3b49b2ca1
fruit-bird <github.ru3w9@slmail.me>
2024-01-03 20:41:35 +0100

    default derives + removal of unused params

--- src/lib.rs
+++ src/lib.rs
@@ -5,57 +5,13 @@ use std::sync::Arc;
 // https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
 // started
 
+#[derive(Debug, Default)]
 struct MidSideSwapper {
     params: Arc<MidSideSwapperParams>,
 }
 
-#[derive(Params)]
-struct MidSideSwapperParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
-    #[id = "gain"]
-    pub gain: FloatParam,
-}
-
-impl Default for MidSideSwapper {
-    fn default() -> Self {
-        Self {
-            params: Arc::new(MidSideSwapperParams::default()),
-        }
-    }
-}
-
-impl Default for MidSideSwapperParams {
-    fn default() -> Self {
-        Self {
-            // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
-            // to treat these kinds of parameters as if we were dealing with decibels. Storing this
-            // as decibels is easier to work with, but requires a conversion for every sample.
-            gain: FloatParam::new(
-                "Gain",
-                util::db_to_gain(0.0),
-                FloatRange::Skewed {
-                    min: util::db_to_gain(-30.0),
-                    max: util::db_to_gain(30.0),
-                    // This makes the range appear as if it was linear when displaying the values as
-                    // decibels
-                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
-                },
-            )
-            // Because the gain parameter is stored as linear gain instead of storing the value as
-            // decibels, we need logarithmic smoothing
-            .with_smoother(SmoothingStyle::Logarithmic(50.0))
-            .with_unit(" dB")
-            // There are many predefined formatters we can use here. If the gain was stored as
-            // decibels instead of as a linear gain value, we could have also used the
-            // `.with_step_size(0.1)` function to get internal rounding.
-            .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-            .with_string_to_value(formatters::s2v_f32_gain_to_db()),
-        }
-    }
-}
+#[derive(Debug, Default, Params)]
+struct MidSideSwapperParams {}
 
 impl Plugin for MidSideSwapper {
     const NAME: &'static str = "Mid Side Swapper";
